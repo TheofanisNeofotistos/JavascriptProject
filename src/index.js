863683123ba1ee99f73c8ab60eb75a11d24f803c
Theofanis Neofotistos <neofotistostheofanis@gmail.com>
2023-07-03 12:14:14 -0400

    created deck with complete images

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 
 // import Example from "./scripts/example";
 import Board from "./scripts/board";
-import Card from "./scripts/card"
+import Card from "./scripts/card";
+import deck from "./scripts/deck";
+
 
 // document.addEventListener("DOMContentLoaded", () => {
 //     const main = document.getElementById("main")
@@ -12,7 +14,18 @@ document.addEventListener("DOMContentLoaded",() => {
     const container = document.getElementById("boardOne")
     const newBoard = new Board(container)
     const hand = document.getElementsByClassName('hand')[0]
-    const newCard = new Card("followUp","grapple","suplex","Woah what a move!",hand,newBoard)
-    const newCard2 = new Card("lead","strike","punch","Woah what a move!",hand,newBoard)
-    const newCard3 = new Card("finish","submission","arm-bar","Woah what a move!",hand,newBoard)
+    // const newCard = new Card("lead","grapple","suplex","Woah what a move!",hand,newBoard)
+
+
+    // let drawnCard = deck[0]
+
+    // new Card(drawnCard.title,drawnCard.type,drawnCard.attribute,drawnCard.img,hand,newBoard)
+    // const newCard2 = new Card("lead","strike","punch","Woah what a move!",hand,newBoard)
+    // const newCard3 = new Card("lead","submission","arm-bar","Woah what a move!",hand,newBoard)
+    // const newCard4 = new Card("followUp","grapple","suplex","Woah what a move!",hand,newBoard)
+    // const newCard5 = new Card("followUp","strike","punch","Woah what a move!",hand,newBoard)
+    // const newCard6 = new Card("followUp","submission","arm-bar","Woah what a move!",hand,newBoard)
+    // const newCard7 = new Card("finish","grapple","suplex","Woah what a move!",hand,newBoard)
+    // const newCard8 = new Card("finish","strike","punch","Woah what a move!",hand,newBoard)
+    // const newCard9 = new Card("finish","submission","arm-bar","Woah what a move!",hand,newBoard)
 })
--- src/scripts/card.js
+++ src/scripts/card.js
@@ -1,9 +1,10 @@
 export default class Card{
-    constructor(type,attribute,title,flavor,container,board){
+    constructor(title,type,attribute,img,container,board){
         this.type = type 
         this.attribute = attribute
         this.title = title 
-        this.flavor = flavor 
+        // this.flavor = flavor 
+        this.img =img
         this.container = container
         this.board = board
         this.handleClick = this.handleClick.bind(this)
@@ -17,6 +18,9 @@ export default class Card{
         card.classList.add('card')
         card.classList.add(this.type)
         card.classList.add(this.attribute)
+
+        card.style.backgroundImage = `url(${this.img})`
+        
        
 
         card.addEventListener("click",this.handleClick)
@@ -28,11 +32,17 @@ export default class Card{
 
     handleClick(e){
         e.preventDefault()
-        debugger
         let selectedCard = e.target
+
+       if( this.board.selected !== undefined){
+        this.board.selected.HTMLElement.classList.remove('selected')
+       }
+
+       this.board.selected = undefined
+
         selectedCard.classList.add('selected')
 
-        
+       
 
        this.board.selected = {HTMLElement: selectedCard , cardObj: this}
       
